e7f1d7a38071204a86c60d0db0b36dbfc2412203
Semyeong Oh <semyeong.oh@gmail.com>
2018-11-02 19:12:37 +0000

    Panels of position and velocity

--- scripts/bokeh_hyades.py
+++ scripts/bokeh_hyades.py
@@ -9,13 +9,13 @@ import astropy.coordinates as coord
 
 from bokeh.io import curdoc
 from bokeh.plotting import figure
-from bokeh.layouts import row, widgetbox
+from bokeh.layouts import row, widgetbox, gridplot
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider
 
 import kinesis
 
-rootdir = '/Users/semyeong/projects/kinesis/'
+rootdir = '/Users/semyeong/projects/kinesis/data/'
 
 hy_tgas = pd.read_csv(rootdir+"reino_tgas_full.csv")
 # full DR2 table for Hyades stars from DR2 van Leeuwen 2018
@@ -25,8 +25,8 @@ hy_tgas = hy_tgas.pipe(kinesis.add_vtan).pipe(kinesis.add_vtan_errors)
 hy_dr2 = hy_dr2.pipe(kinesis.add_vtan).pipe(kinesis.add_vtan_errors)
 
 
-mockhy = kinesis.Cluster([-5.96, 45.60, 5.57], 0.3, omegas=[0,0,0.],
-                         ws=[.1,0,0,0,0],
+mockhy = kinesis.Cluster([0,0,0], 0.3, omegas=[0,0,0.],
+                         ws=[0,0,0,0,0],
                          b0=[17.2, 41.6, 13.6])
 mockhy_members = mockhy.sample_at(kinesis.make_icrs(hy_dr2, coord.ICRS))
 
@@ -35,13 +35,18 @@ source = ColumnDataSource(data=dict(
     dec=hy_dr2.dec,
     parallax=hy_dr2.parallax,
     pmra=mockhy_members.spherical.differentials['s'].d_lon.to(u.mas/u.yr).value,
-    pmdec=mockhy_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value))
+    pmdec=mockhy_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value,
+    vra=mockhy_members.spherical.differentials['s'].d_lon.to(u.mas/u.yr).value/hy_dr2.parallax*4.74,
+    vdec=mockhy_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value/hy_dr2.parallax*4.74,
 
-vmean = [-5.96, 45.60, 5.57]
-vmean = [0, 0, 0]
-vx = Slider(title='vx', value=vmean[0], start=-50, end=50)
-vy = Slider(title='vy', value=vmean[1], start=-50, end=50)
-vz = Slider(title='vz', value=vmean[2], start=-50, end=50)
+    x=mockhy_members.cartesian.x.to(u.pc).value,
+    y=mockhy_members.cartesian.y.to(u.pc).value,
+    z=mockhy_members.cartesian.z.to(u.pc).value
+    ))
+
+vx = Slider(title='vx', value=0, start=-50, end=50)
+vy = Slider(title='vy', value=0, start=-50, end=50)
+vz = Slider(title='vz', value=0, start=-50, end=50)
 sigmav = Slider(title='sigmav', value=0.3, start=0, end=2, step=0.1)
 omegax = Slider(title='omega_x', value=0, start=-1, end=1, step=0.1)
 omegay = Slider(title='omega_y', value=0, start=-1, end=1, step=0.1)
@@ -72,17 +77,24 @@ def update_data(attrname, old, new):
         dec=hy_dr2.dec,
         parallax=hy_dr2.parallax,
         pmra=mockhy_members.spherical.differentials['s'].d_lon.to(u.mas/u.yr).value,
-        pmdec=mockhy_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value)
+        pmdec=mockhy_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value,
+        vra=mockhy_members.spherical.differentials['s'].d_lon.to(u.mas/u.yr).value/hy_dr2.parallax*4.74,
+        vdec=mockhy_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value/hy_dr2.parallax*4.74,
+        x=mockhy_members.cartesian.x.to(u.pc).value,
+        y=mockhy_members.cartesian.y.to(u.pc).value,
+        z=mockhy_members.cartesian.z.to(u.pc).value)
 
 for w in [vx, vy, vz, sigmav, omegax, omegay, omegaz, w1, w2, w3, w4, w5]:
     w.on_change('value', update_data)
 
-hy_dr2_res = hy_dr2.copy()
+hy_dr2_res = hy_dr2.copy().pipe(kinesis.add_xv, coord.ICRS)
 vmean1 = [-5.96, 45.60, 5.57]
 mean_cl = kinesis.Cluster(vmean1, 0)
 mean_cl_members = mean_cl.sample_at(kinesis.make_icrs(hy_dr2, coord.ICRS))
 hy_dr2_res.pmra -= mean_cl_members.spherical.differentials['s'].d_lon.to(u.mas/u.yr).value
 hy_dr2_res.pmdec -= mean_cl_members.spherical.differentials['s'].d_lat.to(u.mas/u.yr).value
+hy_dr2_res['dvra'] = hy_dr2_res.pmra/hy_dr2_res.parallax*4.74
+hy_dr2_res['dvdec'] = hy_dr2_res.pmdec/hy_dr2_res.parallax*4.74
 
 
 p = figure(plot_height=300, plot_width=300)
@@ -109,6 +121,33 @@ p4.yaxis.axis_label = 'pmdec'
 p4.circle('ra', 'pmdec', source=source)
 p4.circle('ra', 'pmdec', source=hy_dr2_res, color='red')
 
-inputs = widgetbox(vx, vy, vz, sigmav, omegax, omegay, omegaz, w1, w2, w3, w4, w5)
-curdoc().add_root(row(inputs, p, p_pm, p3, p4))
+p_zvra = figure(plot_height=300, plot_width=300)
+p_zvra.xaxis.axis_label = 'z'
+p_zvra.yaxis.axis_label = 'vra'
+p_zvra.circle('z', 'vra', source=source)
+p_zvra.circle('z', 'dvra', source=hy_dr2_res, color='red')
 
+p_zvdec = figure(plot_height=300, plot_width=300)
+p_zvdec.xaxis.axis_label = 'z'
+p_zvdec.yaxis.axis_label = 'vdec'
+p_zvdec.circle('z', 'vdec', source=source)
+p_zvdec.circle('z', 'dvdec', source=hy_dr2_res, color='red')
+
+p_xvra = figure(plot_height=300, plot_width=300)
+p_xvra.xaxis.axis_label = 'x'
+p_xvra.yaxis.axis_label = 'vra'
+p_xvra.circle('x', 'vra', source=source)
+p_xvra.circle('x', 'dvra', source=hy_dr2_res, color='red')
+
+p_xvdec = figure(plot_height=300, plot_width=300)
+p_xvdec.xaxis.axis_label = 'x'
+p_xvdec.yaxis.axis_label = 'vdec'
+p_xvdec.circle('x', 'vdec', source=source)
+p_xvdec.circle('x', 'dvdec', source=hy_dr2_res, color='red')
+
+inputs = widgetbox(vx, vy, vz, sigmav, omegax, omegay, omegaz, w1, w2, w3, w4, w5)
+curdoc().add_root(row(inputs, gridplot(
+    [[p, p_pm],
+     [p_zvra, p_zvdec],
+     [p_xvra, p_xvdec]
+     ])))
