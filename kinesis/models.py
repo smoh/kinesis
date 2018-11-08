38b93c4f34b8bdac7570db285c48f3856bf4c40c
Semyeong Oh <semyeong.oh@gmail.com>
2018-11-08 13:57:40 +0000

    Add option to force recompile

--- kinesis/models.py
+++ kinesis/models.py
@@ -17,16 +17,18 @@ def model_cache_path(model_name):
     return os.path.join(ROOT, 'stan', model_name+'.pkl')
 
 
-def get_model(model_name):
+def get_model(model_name, recompile=False):
     """ Get compiled StanModel
     This will compile the stan model if a cached pickle does not exist.
 
     model_name : str,
         model name without `.stan`
+    recompile : bool
+        If True, force recompilation even when cached model exists.
     """
     model_file = model_path(model_name)
     cache_file = model_cache_path(model_name)
-    if not os.path.exists(cache_file):
+    if (not os.path.exists(cache_file)) or recompile:
         model = pystan.StanModel(file=model_file)
         logger.info("Compiling {:s}".format(model_name))
         with open(cache_file, 'wb') as f:
