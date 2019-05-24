39e42580fab3884ad85488416e7416ef6eb98a62
Semyeong Oh <semyeong.oh@gmail.com>
2019-05-24 16:52:42 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 
 from setuptools import find_packages, setup, Command
 
-NAME = 'kinesis'
-DESCRIPTION = 'Kinematic modelling of comoving stars'
-AUTHOR = 'Semyeong Oh'
-EMAIL =  'semyeong.oh@gmail.com'
+NAME = "kinesis"
+DESCRIPTION = "Kinematic modelling of comoving stars"
+AUTHOR = "Semyeong Oh"
+EMAIL = "semyeong.oh@gmail.com"
 
 setup(
     name=NAME,
     description=DESCRIPTION,
     author=AUTHOR,
     author_email=EMAIL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["astropy", "arviz", "pystan==2.18"],
 )
