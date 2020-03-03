92173804897a57f8e5c6bee3e812faa1e87d2688
Semyeong Oh <semyeong.oh@gmail.com>
2020-03-03 15:46:53 +0000

    take care of paths

--- kinesis/data.py
+++ kinesis/data.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 
@@ -7,7 +8,8 @@ import astropy.coordinates as coord
 
 import gapipes
 
-DATADIR = "/Users/semyeong/projects/kinesis/data/"
+package_directory = os.path.dirname(os.path.abspath(__file__))
+data_directory = os.path.join(os.path.dirname(package_directory), "data")
 
 
 __all__ = ["load_reino2018_supp", "load_reino2018_full", "load_hyades_dataset"]
@@ -15,7 +17,7 @@ __all__ = ["load_reino2018_supp", "load_reino2018_full", "load_hyades_dataset"]
 
 def load_reino2018_supp():
     return pd.read_csv(
-        DATADIR + "Reino2018_supp.txt",
+        data_directory + "/Reino2018_supp.txt",
         comment="#",
         delim_whitespace=True,
         dtype={"HIP": str, "TYC": str, "source_id": str},
@@ -23,11 +25,11 @@ def load_reino2018_supp():
 
 
 def load_reino2018_full():
-    return pd.read_csv(DATADIR + "reino_tgas_full.csv")
+    return pd.read_csv(data_directory + "/reino_tgas_full.csv")
 
 
 def load_hyades_dataset():
-    df = pd.read_csv("../data/hyades_full.csv")
+    df = pd.read_csv(data_directory + "/hyades_full.csv")
     df["x"], df["y"], df["z"] = df.g.icrs.cartesian.xyz.value
     df["vx"], df["vy"], df["vz"] = df.g.icrs.velocity.d_xyz.value
     df["gx"], df["gy"], df["gz"] = df.g.galactic.cartesian.xyz.value
