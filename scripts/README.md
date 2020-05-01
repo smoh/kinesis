18aea14e52be7d364cca89027c2f486030f3a869
Semyeong Oh <semyeong.oh@gmail.com>
2020-05-01 13:00:16 +0100

    sub versions

--- scripts/README.md
+++ scripts/README.md
@@ -4,3 +4,15 @@ python fit-hyades.py -r 10 1000 --v0 -6.086 45.628 5.517 myfit_10_m_fixed_v0
 python fit-hyades.py -r 5 15 --v0 -6.103 45.611 5.532 myfit_5_15_fixed_v0.pickle
 python fit-hyades.py -r 10 1000 myfit_10_m.pickle
 python fit-hyades.py -r 0 10 myfit_0_10
+
+
+python hyades-mock-fullmodel.py mock_null.pickle
+
+
+
+python fit-hyades.py --exclude-other -r 0 10 hyades_0_10
+python fit-hyades.py --exclude-other -r 10 1000 hyades_10_m
+python fit-hyades.py --exclude-other -r 10 1000 --v0 -6.08588564 45.62882666  5.51807565 hyades_10_m_fixed_v0
+
+# create result summary table for paper
+$ python make-table.py --latex --fit cl hyades_0_10.pickle --fit tails hyades_10_m_fixed_v0.pickle
--- scripts/determine-cluster-center.py
+++ scripts/determine-cluster-center.py
@@ -25,7 +25,7 @@ def xyz_icrs_to_galactic(xyz):
 
 df = kn.data.load_hyades_dataset()
 df = df.loc[df['Member_r19']!='other'].reset_index(drop=True)
-print(len(df))
+print(len(df), 'rows')
 cl_gaia = df.loc[df["in_dr2"] == True].copy()
 
 b_c_icrs_cl_gaia_mean = cl_gaia.g.icrs.cartesian.xyz.mean(axis=1).value
@@ -138,3 +138,10 @@ ax_xz.set_ylabel("$z$ [pc]")
 fig.suptitle("Galactic")
 fig.tight_layout()
 fig.savefig("../report/galactic_xyz.pdf")
+
+N_rv = (df['radial_velocity'].notna()).sum()
+N_10 = (r_c<10).sum()
+N_10_rv = ((r_c<10) & (df['radial_velocity'].notna())).sum()
+print('N rv =', N_rv)
+print('N(r_c<10) = {} N(r_c<10 and has rv) = {}'.format(N_10, N_10_rv))
+print('N(r_c>10) = {} N(r_c<10 and has rv) = {}'.format(len(df)-N_10, N_rv-N_10_rv))
