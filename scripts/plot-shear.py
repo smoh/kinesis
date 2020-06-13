30471ad9b6b8f87c1f09b33da7d4be60830feaa9
Semyeong Oh <semyeong.oh@gmail.com>
2020-06-13 17:51:54 +0100

    update; move to 2d to simplify things

--- scripts/plot-shear.py
+++ scripts/plot-shear.py
@@ -14,22 +14,60 @@ import astropy.units as u
 
 kn.set_mpl_style()
 
-T = np.random.uniform(size=(3, 3))
-T = (T.T + T) / 2.0
+# # generate a random symmetrix matrix
+# T = np.random.uniform(size=(2, 2))
+# T = (T.T + T) / 2.0
 
-b0 = np.array([10, 10, 10])
-cl = kn.Cluster([0, 0, 0], 0.0, T=T, b0=b0)
+T = np.array([[0.65896236, 0.78559313], [0.78559313, 0.33118707]])
+
 # sample at grids
-x, y, z = np.meshgrid(*[np.linspace(-5, 5, 5)] * 3)
-x, y, z = x.ravel(), y.ravel(), z.ravel()
-x, y, z = x + b0[0], y + b0[1], z + b0[2]
-pos = coord.ICRS(x * u.pc, y * u.pc, z * u.pc, representation_type="cartesian")
-cl.sample_at(pos)
+x, y = np.meshgrid(*[np.linspace(-5, 5, 11)] * 2)
+x, y = x.ravel(), y.ravel()
+X = np.vstack([x, y])
+V = np.einsum("ji,in->jn", T, X)
 
-g = cl.members.truth.g
-v = g.icrs.velocity.d_xyz.value
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 3))
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), subplot_kw={"projection": "3d"})
-ax1.quiver(x, y, z, v[0], v[1], v[2])
+ax1.quiver(*X, *V)
 
-fig.savefig("../report/rotating-shear.png")
+ax1.spines["right"].set_visible(False)
+ax1.spines["top"].set_visible(False)
+ax1.set_xticks([])
+ax1.set_yticks([])
+
+first_axis_color = "tab:red"
+second_axis_color = "tab:blue"
+
+ax2.spines["right"].set_visible(False)
+ax2.spines["top"].set_visible(False)
+ax2.spines["bottom"].set(linewidth=2, color=first_axis_color)
+ax2.spines["left"].set(linewidth=2, color=second_axis_color)
+ax2.set_xticks([])
+ax2.set_yticks([])
+
+# rotate to frame defined by principal axes of shear
+W, R = np.linalg.eig(T)
+invR = R.T
+
+# indicate principal axes
+length = 2
+ax1.plot([0, R[0, 0] * length], [0, R[1, 0] * length], color=first_axis_color, lw=2)
+ax1.plot([0, R[0, 1] * length], [0, R[1, 1] * length], color=second_axis_color, lw=2)
+
+# Xnew = np.einsum("ij,jn->in", invR, X)
+# Vnew = np.einsum("ij,jn->in", invR, V)
+# ax2.quiver(*Xnew, *Vnew)
+
+
+x, y = np.meshgrid(*[np.linspace(-5, 5, 11)] * 2)
+x, y = x.ravel(), y.ravel()
+Xnew = np.vstack([x, y])
+Vnew = np.einsum("ji,in->jn", W * np.eye(2), Xnew)
+Vnew = np.einsum("ji,in->jn", invR @ T @ R, Xnew)
+ax2.quiver(*Xnew, *Vnew)
+
+ax1.axis([-7, 7, -7, 7])
+ax2.axis([-7, 7, -7, 7])
+
+fig.tight_layout()
+fig.savefig("../report/rotating-shear.pdf")
