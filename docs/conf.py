ef9f655abd299d006f13e27cd76adf23eefe54f1
Semyeong Oh <semyeong.oh@gmail.com>
2021-03-14 12:12:47 +0000

    Try furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = "kinesis"
-copyright = "2019-2020, Semyeong Oh"
+copyright = "2019-2021, Semyeong Oh"
 author = "Semyeong Oh"
 
 
@@ -46,8 +46,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-# html_theme = "alabaster"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
