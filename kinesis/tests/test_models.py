f81f8ed6314ec1f49caa0476d38eca209a8481ab
Semyeong Oh <semyeong.oh@gmail.com>
2021-03-15 14:02:20 +0000

    A simple test case with new interface

--- kinesis/tests/test_models.py
+++ kinesis/tests/test_models.py
@@ -2,17 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pytest
-import astropy.coordinates as coord
-import astropy.units as u
-
+import pandas as pd
 import kinesis as kn
 import pystan
-import gapipes as gp
 
 np.random.seed(73929)
 
 
-def test_compile_general_model():
-    model = kn.get_model("general_model")
-    assert isinstance(model, pystan.StanModel)
+# def test_models_compile():
+#     for model_name in kn.models.available:
+#         model = kn.get_model(model_name, recompile=True)
+#         assert isinstance(model, pystan.StanModel)
+
+
+def test_model():
+    N, Nbg = 40, 10  # number of sources
+    b0 = np.array([17.7, 41.2, 13.3])  # pc
+    v0 = np.array([-6.32, 45.24, 5.30])  # [vx, vy, vz] in km/s
+    sigv = 0.0  # dispersion, km/s
+    cl = (
+        kn.Cluster(v0, sigv, b0=b0, omegas=[100, 0, 200])
+        .sample_sphere(N=N, Rmax=5)
+        .observe(cov=np.eye(3) * 0.01, rv_error=np.ones(N))
+    )
+    bg = (
+        kn.Cluster(v0, 10.0, b0=b0)
+        .sample_sphere(N=Nbg, Rmax=5)
+        .observe(cov=np.eye(3) * 0.01, rv_error=np.ones(Nbg))
+    )
+    df = pd.concat((cl.members.observed, bg.members.observed), axis=0)
+    m = kn.AllCombined(recompile=True)
+
+    def initfunc():
+        return dict(
+            v0=np.array(v0) + np.random.normal(size=3),
+            sigv=[2, 2, 2],
+            sigv_bg=50,
+            f_mem=0.6,
+        )
+
+    fit = m.fit(df, b0=b0, init=initfunc)
